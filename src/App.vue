8604a099ceef47edc0fc9f2263b958a5f918c0da
Vicente Aburto <102199491+VicenteAB@users.noreply.github.com>
2022-11-01 16:24:21 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import users from '../users.json'
+</script>
+
+<template>
+  <div class="container" >
+       <div class="row">
+          <div class="col-md-12">
+             <div class="table-responsive">
+                <table class="table">
+                   <thead class="thead-dark">
+                      <tr>
+                         <th scope="col">Nombre</th>
+                         <br>
+                         <th scope="col">Genero</th>
+                         <br>
+                         <th scope="col">Pais</th>
+                         <br>
+                         <th scope="col">Imagen</th>
+                         <!--Holaaa-->
+                      </tr>
+                   </thead>
+                   <tbody>
+                      <tr v-for="users in users" v-bind:key="users.gender">
+                         <td>{{users.name.title}},{{users.name.first}},{{users.name.last}}</td>
+                         <br>
+                         <td>{{users.gender}}</td>
+                         <br>
+                         <td>{{users.location.country}}</td>
+                      </tr>
+                   </tbody>
+                </table>
+  
+             </div>
+          </div>
+       </div>
+    </div>
+    
+</template>
+
+<style scoped>
+
+</style>
